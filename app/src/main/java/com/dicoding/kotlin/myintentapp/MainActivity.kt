a26c6d11f0d62a0e142307211888676ae90846f2
adeliaayu <adeliaayut53@gmail.com>
2020-08-05 20:22:27 +0700

    add move activity with object

--- app/src/main/java/com/dicoding/kotlin/myintentapp/MainActivity.kt
+++ app/src/main/java/com/dicoding/kotlin/myintentapp/MainActivity.kt
@@ -18,6 +18,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val btnMoveWithDataActivity: Button = findViewById(R.id.btn_move_activity_data)
         btnMoveWithDataActivity.setOnClickListener(this)
 
+        val btnMoveWithObject: Button = findViewById(R.id.btn_move_activity_object)
+        btnMoveWithObject.setOnClickListener(this)
+
         val btnDialPhone: Button = findViewById(R.id.btn_dial_number)
         btnDialPhone.setOnClickListener(this)
     }
@@ -34,6 +37,19 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "Girls Who Code")
                 moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, "17")
                 startActivity(moveWithDataIntent)
+            }
+
+            R.id.btn_move_activity_object -> {
+                val person = Person(
+                    "DicodingAcademy",
+                    5,
+                    "academy@dicoding.com",
+                    "Bandung"
+                )
+
+                val moveWithObjectIntent = Intent(this@MainActivity, MoveWithObjectActivity::class.java)
+                moveWithObjectIntent.putExtra(MoveWithObjectActivity.EXTRA_PERSON, person)
+                startActivity(moveWithObjectIntent)
             }
 
             R.id.btn_dial_number -> {
